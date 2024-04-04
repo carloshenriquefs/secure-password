adcc5724ad6bff7dc21d08dbbd4ef2c14e6b7046
Carlos Henrique <ichfs9108@gmail.com>
2024-04-04 16:33:58 -0300

    feat: Adicionando controller

---
+++ securepassword/src/main/java/tech/buildrun/securepassword/controller/ApiController.java
@@ -0,0 +1,29 @@
+package tech.buildrun.securepassword.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import tech.buildrun.securepassword.service.PasswordService;
+
+@RestController
+public class ApiController {
+
+    private final PasswordService passwordService;
+
+    public ApiController(PasswordService passwordService) {
+        this.passwordService = passwordService;
+    }
+
+    @PostMapping(value = "/validate-password")
+    public ResponseEntity<FailureResponse> validatePassword(@RequestBody BodyRequest bodyRequest) {
+
+        var failures = passwordService.validatePass(bodyRequest.password());
+
+        if (failures.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+
+        return ResponseEntity.badRequest().body(new FailureResponse(failures));
+    }
+}
